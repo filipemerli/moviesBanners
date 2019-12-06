388ee2fe89b489b4d0b0854103638ed512715057
Filipe Merli <filipemerli@air-de-filipe.home>
2019-12-05 22:20:39 -0300

    XCode Project created

---
+++ SimpleBanners/SimpleBanners/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleBanners
+//
+//  Created by Filipe Merli on 05/12/19.
+//  Copyright © 2019 Filipe Merli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
